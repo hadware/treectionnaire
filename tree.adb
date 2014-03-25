b6c3b9f371b178d5b69df8bf19f69247709ec743
Thomas <thomas>
2014-03-25 20:19:55 +0100

    fonction new tree. structure de l'arbre

--- tree.adb
+++ tree.adb
@@ -1,22 +1,47 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+with Ada.Strings.Unbounded;
+use Ada.Strings.Unbounded;
 
 
-
--- retourne le nombre de lettre dans le mot
-function Nomber_Letter (Word : out String, Letter : in out String) return Natural is
-begin
+package body Tree is
+   type Letter_Counter is array (1..26) of Integer;
    
-end;
-
--- insere un mot dans l'arbre
-procedure Insertion (T : in out Tree ; Word : in String ) is
+   -- creer le premier niveau de l'arbre , le noeud racine
+   function New_Tree return Tree is
+      Root : Tree;
+   begin
+      Root := new Node;
+      return Root ;   
+   end;
    
-begin
+
+   -- retourne le nombre de lettre dans le mot
+   function Nomber_Letter (Word : String) return Letter_counter is
+      Nb_Array : Letter_Counter;   
+   begin
+      -- a = 97    z= 122
+     
       
-   
-end;
+      null;
+      
+      return Nb_Array;
+   end;
 
--- recherche les mots 
-procedure Search_And_Display (T : in Tree ; Letters : in String) is
-begin
+   -- insere un mot dans l'arbre
+   procedure Insertion (T : in out Tree ; Word : in String ) is
    
-end;
+   begin
+      
+   null;
+   end;
+
+   -- recherche les mots 
+   procedure Search_And_Display (T : in Tree ; Letters : in String) is
+   begin
+   null;
+   
+   
+   end;
+   
+end ;
--- tree.ads
+++ tree.ads
@@ -3,13 +3,36 @@ use Ada.Strings.Unbounded;
 
 package Tree is
 
-	type Tree is private;
-	function New_Tree return Tree;
-	procedure Insertion(T : in out Tree ; Word : in String);
-	procedure Search_And_Display(T : in Tree ; Letters : in String);
+   type Tree is private;
+   function New_Tree return Tree;
+   procedure Insertion(T : in out Tree ; Word : in String);
+   procedure Search_And_Display(T : in Tree ; Letters : in String);
 
 private
+   
+  
+   type Forest is array (0..8) of Tree;
+   type Node_Nature is ( Is_Node, Is_Leaf);
+   type Tag_Node is record
+      Node_Letter: Character;
+      Node_Branch_Array : Forest; 
+   end record;
+      
+   type Tag_Leaf is record
+      --Contient une double liste des mots contenus par la feuille
+      Truc : Integer;
+   end record;
+   
+   
+   
+   type Node is record
+      Node_Type: Node_Nature;
+      Node_Tag_Ptr : access Tag_Node;
+      Leaf_Tag_Ptr: access Tag_Leaf;
+   end record;
+   type Tree is access Node;
+   
+          
 
-	type Node;
-	type Tree is access Node;
+   
 end Tree;
