c066258d9ba03d928c248fedd60345dfdc98b21c
Thomas <thomas>
2014-03-25 18:19:10 +0100

    machin truc

---
+++ tree.adb
@@ -0,0 +1,22 @@
+
+
+
+-- retourne le nombre de lettre dans le mot
+function Nomber_Letter (Word : out String, Letter : in out String) return Natural is
+begin
+   
+end;
+
+-- insere un mot dans l'arbre
+procedure Insertion (T : in out Tree ; Word : in String ) is
+   
+begin
+      
+   
+end;
+
+-- recherche les mots 
+procedure Search_And_Display (T : in Tree ; Letters : in String) is
+begin
+   
+end;
