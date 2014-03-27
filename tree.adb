8c3e9f3379f5d718b1c2caa17deeb3089d299b2c
Thomas <thomas>
2014-03-27 18:27:45 +0100

    codage fonction nomber_letter et ca marche. debut du codage de la procedure insertion

--- tree.adb
+++ tree.adb
@@ -3,9 +3,10 @@ use Ada.Text_IO;
 with Ada.Strings.Unbounded;
 use Ada.Strings.Unbounded;
 
-
+with Ada.Integer_Text_Io;
+use Ada.Integer_Text_Io;
 package body Tree is
-   type Letter_Counter is array (1..26) of Integer;
+   --type Letter_Counter is array (97..122) of Integer;
    
    -- creer le premier niveau de l'arbre , le noeud racine
    function New_Tree return Tree is
@@ -15,26 +16,60 @@ package body Tree is
       return Root ;   
    end;
    
-
+   ----------------------------------------------------------------------
    -- retourne le nombre de lettre dans le mot
    function Nomber_Letter (Word : String) return Letter_counter is
       Nb_Array : Letter_Counter;   
    begin
       -- a = 97    z= 122
-     
-      
-      null;
-      
+      Nb_Array := (others => 0);
+      for I in Word'range loop
+	 Nb_Array (Character'pos(Word(I))) := Nb_Array (Character'pos( Word(I))) + 1;
+      end loop;
       return Nb_Array;
-   end;
-
+   end Nomber_Letter;
+   
+   ----------------------------------------------------------------------
+   -- selection le noeud suivant en fonction du nb de lettre du niveau 
+   function Select_Forest (P : Tree ; J : Integer ; Nb_Array : Letter_counter) return Tree is
+      Nb_letter : Integer; --nb de lettre de 'J' ds le mot
+   begin
+      Nb_Letter := Nb_Array( J + 97);
+      return P.Node_Tag_Ptr.all.Node_Branch_Array(Nb_letter);
+   end Select_forest;
+   
+   ----------------------------------------------------------------------
    -- insere un mot dans l'arbre
    procedure Insertion (T : in out Tree ; Word : in String ) is
-   
-   begin
+      P : Tree;
+      Nb_Array : Letter_Counter;
+      Node_Array : Forest;
+            
+      --creer un nouveau noeud
+      procedure Creat_Node(P :  Tree ; J : integer) is
+	 New_Node : node;
+	 New_Array : Forest;
+      begin
+	 New_Node.Node_Type := Is_Node;
+	 P.all := New_Node;
+	 New_Node.Node_Tag_Ptr.all.Node_Letter := Character'Val(J + 97) ;
+	 New_Node.Node_Tag_Ptr.all.Node_Branch_Array := new_Array;
+      end Creat_Node;
       
-   null;
-   end;
+   begin
+      Nb_Array:=Nomber_Letter(Word);
+      
+      for J in 1..27 loop
+	 Put(J);
+	 if (P = null) then
+	    Creat_Node (P,J);
+	    P := Select_Forest(P, J, Nb_Array);
+	 else 
+	    P := Select_Forest(P, J, Nb_Array);
+	 end if;
+      end loop;
+      
+   end Insertion;
 
    -- recherche les mots 
    procedure Search_And_Display (T : in Tree ; Letters : in String) is
@@ -42,6 +77,6 @@ package body Tree is
    null;
    
    
-   end;
+   end Search_And_Display;
    
 end ;
