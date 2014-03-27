c44bb99114538dc708cb059621d8768394de63c8
Thomas <thomas>
2014-03-27 22:27:21 +0100

    no comment

--- tree.adb
+++ tree.adb
@@ -8,6 +8,7 @@ use Ada.Integer_Text_Io;
 package body Tree is
    --type Letter_Counter is array (97..122) of Integer;
    
+   
    -- creer le premier niveau de l'arbre , le noeud racine
    function New_Tree return Tree is
       Root : Tree;
@@ -18,7 +19,7 @@ package body Tree is
    
    ----------------------------------------------------------------------
    -- retourne le nombre de lettre dans le mot
-   function Nomber_Letter (Word : String) return Letter_counter is
+   function Count_Letters (Word : String) return Letter_counter is
       Nb_Array : Letter_Counter;   
    begin
       -- a = 97    z= 122
@@ -27,46 +28,55 @@ package body Tree is
 	 Nb_Array (Character'pos(Word(I))) := Nb_Array (Character'pos( Word(I))) + 1;
       end loop;
       return Nb_Array;
-   end Nomber_Letter;
+   end Count_Letters;
    
    ----------------------------------------------------------------------
    -- selection le noeud suivant en fonction du nb de lettre du niveau 
-   function Select_Forest (P : Tree ; J : Integer ; Nb_Array : Letter_counter) return Tree is
+   function Select_Forest (Buffer_node : Tree ; J : Integer ; Nb_Array : Letter_counter) return Tree is
       Nb_letter : Integer; --nb de lettre de 'J' ds le mot
    begin
-      Nb_Letter := Nb_Array( J + 97);
-      return P.Node_Tag_Ptr.all.Node_Branch_Array(Nb_letter);
+      Nb_Letter := Nb_Array( J + 97); Put(Nb_Letter);
+      return Buffer_node.Node_Tag_Ptr.all.Node_Branch_Array(Nb_letter);
    end Select_forest;
+   
+   --==========================================================================
+   -- Ajoute un mot à la feuille passée en argument
+   --==========================================================================
+   procedure Add_Word_To_Leaf(Word_To_Add : in Unbounded_String; Input_Leaf: in out Tag_Leaf) is
+   begin
+      Word_List.Append(Input_Leaf.Words, Word_To_Add);
+   end Add_Word_To_Leaf; 
+   --==========================================================================
+   
    
    ----------------------------------------------------------------------
    -- insere un mot dans l'arbre
    procedure Insertion (T : in out Tree ; Word : in String ) is
-      P : Tree;
+      Buffer_node : Tree;
       Nb_Array : Letter_Counter;
       Node_Array : Forest;
             
       --creer un nouveau noeud
-      procedure Creat_Node(P : Tree ; J : integer) is
-	 --New_Node : Node;
+      procedure Create_Node(Buffer_node : out Tree ; J : integer) is
 	 New_Array : Forest;
       begin
-	 
-	 P.Node_Type := Is_Node;
-	 --P.all := New_Node;
-	 P.Node_Tag_Ptr.all.Node_Letter := Character'Val(J + 97) ;
-	 P.Node_Tag_Ptr.all.Node_Branch_Array := new_Array;
-      end Creat_Node;
+	 New_Array:= (others => null);
+	 Buffer_node := new Node;
+	 Buffer_node.Node_Type := Is_Node;
+	 Buffer_node.Node_Tag_Ptr.all.Node_Letter := Character'Val(J + 97) ;
+	 Buffer_node.Node_Tag_Ptr.all.Node_Branch_Array := new_Array;
+      end Create_Node;
       
    begin
-      Nb_Array:=Nomber_Letter(Word);
-      
+      Nb_Array := Count_Letters(Word);
+      Buffer_Node := T;
       for J in 0..26 loop
 	 Put(J);
-	 if (P = null) then
-	    Creat_Node (P,J);
-	    P := Select_Forest(P, J, Nb_Array);
+	 if (Buffer_Node = null) then
+	    Create_Node (Buffer_Node, J);
+	    Buffer_node := Select_Forest(Buffer_node, J, Nb_Array);
 	 else 
-	    P := Select_Forest(P, J, Nb_Array);
+	    Buffer_node := Select_Forest(Buffer_node, J, Nb_Array);
 	 end if;
       end loop;
       
--- tree.ads
+++ tree.ads
@@ -1,38 +1,39 @@
 with Ada.Strings.Unbounded;
 use Ada.Strings.Unbounded;
-
+with Ada.Containers.Doubly_Linked_Lists;
 package Tree is
-
-   type Tree is private;
-   function New_Tree return Tree;
-   procedure Insertion(T : in out Tree ; Word : in String);
-   procedure Search_And_Display(T : in Tree ; Letters : in String);
-
-private
-   
   
-   type Forest is array (0..8) of Tree;
-   type Node_Nature is ( Is_Node, Is_Leaf);
-   type Tag_Node is record
-      Node_Letter: Character;
+  
+  
+  type Tree is private;
+  function New_Tree return Tree;
+  procedure Insertion(T : in out Tree ; Word : in String);
+  procedure Search_And_Display(T : in Tree ; Letters : in String);
+  
+  type Letter_Counter is array (97..122) Of Integer;
+  function Count_Letters ( Word : String) return Letter_Counter;
+  
+  private
+     
+     type Forest is array (0..8) of Tree;
+     type Node_Nature is ( Is_Node, Is_Leaf);
+     type Tag_Node is record
+	Node_Letter: Character;
       Node_Branch_Array : Forest; 
-   end record;
-      
-   type Tag_Leaf is record
-      --Contient une double liste des mots contenus par la feuille
-      Truc : Integer;
-   end record;
-   
-   
-   
-   type Node is record
-      Node_Type: Node_Nature;
-      Node_Tag_Ptr : access Tag_Node;
-      Leaf_Tag_Ptr: access Tag_Leaf;
-   end record;
-   type Tree is access Node;
-   
-          
-
-   
+     end record;
+     
+     package Word_List is new Ada.Containers.Doubly_Linked_Lists(Unbounded_String);
+     
+     type Tag_Leaf is record
+	--Contient une double liste des mots contenus par la feuille
+	Words : Word_List.List;
+     end record;
+     
+     type Node is record
+	Node_Type: Node_Nature; --défini si le noeud est un noeud standard ou une feuille
+      Node_Tag_Ptr : access Tag_Node; --utilisé si le noeud est standard
+      Leaf_Tag_Ptr: access Tag_Leaf; --utilisé si le noeud est une feuille 
+     end record;
+     type Tree is access Node;
+        
 end Tree;
