1bceb4591b6f847f39eecaf6e802e5e4072d965c
hadware <hadwarez@gmail.com>
2014-04-02 22:42:48 +0200

    Ajout d'arguments modifiables pour la génération aléatoire

--- test_time.adb
+++ test_time.adb
@@ -1,45 +1,60 @@
 with Tree; use Tree;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Numerics.Discrete_Random;
+with Ada.Command_Line; use Ada.Command_Line;
+
 procedure Test_time is
    function Read_Words_File(File_Name: in String) return Tree.Tree is
       File : File_Type;
       T : Tree.Tree := New_Tree;
       
-	begin
+   begin
       Open(File, In_File, File_Name);
-	  while not End_Of_File(File) loop
-		  declare
-			  Line : String := Get_Line(File);
-		  begin
-			  Insertion(T, Line);
-		  end;
-	  end loop;
-	  Close(File);
-	  return T;
-	end Read_Words_File;
+      while not End_Of_File(File) loop
+	 declare
+	    Line : String := Get_Line(File);
+	 begin
+	    Insertion(T, Line);
+	 end;
+      end loop;
+      Close(File);
+      return T;
+   end Read_Words_File;
 	
-	subtype Letters is Character range 'a'..'z'; 
-	package Random_Letters is new Ada.Numerics.Discrete_Random(Letters);
-	use Random_Letters;
-	G: Generator;
-	
-	T : Tree.Tree;
-	word : String (1..10);
-	Nb_Word, Length_Word : Integer;
-      
+   subtype Letters is Character range 'a'..'z'; 
+   package Random_Letters is new Ada.Numerics.Discrete_Random(Letters);
+   use Random_Letters;
+   G: Generator;
+   
+   T : Tree.Tree;
+   
+   -- Fichier qui stocke les mots générés aléatoirement
+   Random_Word_File : File_Type;
+   
+   -- Paramètres de génération du dico aléatoire
+   Word_Length : Positive := 10;
+   Word_Number : Positive := 1000;
+   
 begin
    T := Read_Words_File("american-english");
    Reset(G);
-   Nb_Word := 1000;
-   Length_Word := 10;
-   for I in 1..Nb_word loop
-      for J in 1..Length_Word loop
-	 
-	 Word(J):= Random(G);
+   
+   if Argument_Count = 2 then
+      Word_Length := Integer'Value(Argument(1));
+      Word_Number := Integer'Value(Argument(2));
+   end if;
+   
+   declare
+      Word : String(1..Word_Number);
+   begin
+      for I in 1..Word_Number loop
+	 for J in 1..Word_Length loop
+	    Word(J):= Random(G);
+	 end loop;
+	 Put_Line(Word);
+	 Search_And_Display(T,Word);
       end loop;
-      Put_Line(Word);
-      Search_And_Display(T,Word);
-   end loop;
+   end;
+   
 end;
 
